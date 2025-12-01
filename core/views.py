7fb9046ebc62e7d23b519a62d45080a3086efe65
AOKTunbosun <aokolatunbosun@gmail.com>
2025-12-01 12:13:14 +0100

    Started on the landing page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+class LandingPage(View):
+    def get(self, request):
+        
+        context = {}
+        return render(request, 'core/landing.html', context)
