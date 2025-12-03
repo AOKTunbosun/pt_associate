c4c47de50163d273a70dbf7bd87fd400c39ce6a2
AOKTunbosun <aokolatunbosun@gmail.com>
2025-12-03 19:19:05 +0100

    Created the Teacher profile table

--- core/models.py
+++ core/models.py
@@ -20,3 +20,12 @@ class ParentProfile(models.Model):
 
     def __str__(self):
         return f"{self.user_id.first_name} {self.user_id.last_name}'s parent profile"
+
+
+class TeacherProfile(models.Model):
+    id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user_id.first_name} {self.user_id.last_name}'s teacher profile"
+
