2f1ce32ef851bb155e6316755d49ea74a6237ec1
AOKTunbosun <aokolatunbosun@gmail.com>
2025-12-04 11:15:15 +0100

    Added the requirements.txt

--- core/views.py
+++ core/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib import messages
 
 # Create your views here.
+User = get_user_model()
+
 
 class LandingPage(View):
     def get(self, request):
@@ -17,10 +21,34 @@ class LoginPage(View):
         return render(request, 'core/login.html', context)
     
     def post(self, request):
-        print(request.POST.get('email'))
-        context = {}
-        return render(request, 'core/login.html', context)
+        if request.user.is_authenticated:
+            logout(request)
+        
+        email = request.POST.get('email').strip()
+        password = request.POST.get('password').strip()
+        remember = request.POST.get('remember')
 
+        try:
+            user = User.objects.get(email=email)
+        
+        except:
+            messages.error(request, message='User does not exist, try signing up')
+            return redirect('login')
+        
+        user = authenticate(request, email=email, password=password)
+
+
+        if not user:
+            messages.error(request, message='Incorrect password')
+            return redirect('login')
+
+        else:
+            login(request, user)
+            if not remember:
+                request.session.set_expiry(0)
+            else:
+                request.session.set_expiry(60*60*24*30)
+            return redirect('login')
 
 
 class SignupPage(View):
@@ -32,3 +60,4 @@ class SignupPage(View):
         print(request.POST.get('firstName'))
         context = {}
         return render(request, 'core/signup.html', context)
+
